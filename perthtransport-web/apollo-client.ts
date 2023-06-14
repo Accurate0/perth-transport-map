9256c7b1367db5182279fed224e2950ca4424580
Anurag Singh <contact@anurag.sh>
2023-06-14 18:12:42 +0800

    huh

--- perthtransport-web/apollo-client.ts
+++
@@ -1,6 +0,0 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-
-const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_BASE,
-  cache: new InMemoryCache(),
-});
--- perthtransport-web/app/map/page.tsx
+++ perthtransport-web/app/map/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return null;
+import { getClient } from "@/lib/apollo-client";
+import { gql } from "@apollo/client";
+
+export default async function Home() {
+  const { data } = await getClient().query({
+    query: gql`
+      {
+        route(searchTerm: "arma") {
+          routes {
+            identifier
+            timetableId
+          }
+        }
+        liveTrips(timetableId: "PerthRestricted:RTG_10") {
+          liveTrips
+        }
+      }
+    `,
+  });
+
+  return <></>;
 }
