5524d7f788f558ccd2450a48ffbae7065faf816f
Anurag Singh <contact@anurag.sh>
2023-06-24 21:20:42 +0800

    simplify health status

--- perthtransport-api/src/routes/health.rs
+++ perthtransport-api/src/routes/health.rs
@@ -1,4 +1,5 @@
 use crate::types::{AppState, ServiceUnavailable};
+use anyhow::Context;
 use axum::extract::State;
 use futures_util::StreamExt;
 use http::StatusCode;
@@ -18,39 +19,26 @@ pub async fn health_check(State(state): State<AppState>) -> Result<StatusCode, S
         .publish(PUBSUB_CHANNEL_WORKER_HEALTH_IN, "hey")
         .await?;
 
+    let worker_health = tokio::time::timeout(Duration::from_secs(5), pubsub.on_message().next())
+        .await?
+        .context("must contain message")?;
+
     let worker_health =
-        tokio::time::timeout(Duration::from_secs(5), pubsub.on_message().next()).await;
+        serde_json::from_str::<WorkerHealthStatus>(&worker_health.get_payload::<String>()?)?;
 
-    match worker_health {
-        Ok(r) => match r {
-            Some(m) => {
-                let worker_health =
-                    serde_json::from_str::<WorkerHealthStatus>(&m.get_payload::<String>()?)?;
+    if worker_health.task_manager_healthy
+        && worker_health.worker_output_healthy
+        && worker_health.active_trains_healthy
+    {
+        Ok(StatusCode::NO_CONTENT)
+    } else {
+        tracing::error!(
+            "task_manager: {}, worker_output: {}, active_trains: {}",
+            worker_health.task_manager_healthy,
+            worker_health.worker_output_healthy,
+            worker_health.active_trains_healthy
+        );
 
-                if worker_health.task_manager_healthy
-                    && worker_health.worker_output_healthy
-                    && worker_health.active_trains_healthy
-                {
-                    Ok(StatusCode::NO_CONTENT)
-                } else {
-                    tracing::error!(
-                        "task_manager: {}, worker_output: {}, active_trains: {}",
-                        worker_health.task_manager_healthy,
-                        worker_health.worker_output_healthy,
-                        worker_health.active_trains_healthy
-                    );
-
-                    Ok(StatusCode::SERVICE_UNAVAILABLE)
-                }
-            }
-            None => {
-                tracing::error!("received message but it's empty");
-                Ok(StatusCode::SERVICE_UNAVAILABLE)
-            }
-        },
-        Err(_) => {
-            tracing::error!("time out in worker health check");
-            Ok(StatusCode::SERVICE_UNAVAILABLE)
-        }
+        Ok(StatusCode::SERVICE_UNAVAILABLE)
     }
 }
