2b261c62f653652391402536efc91efb8b96abd4
Anurag Singh <contact@anurag.sh>
2023-07-01 23:00:06 +0800

    add comment

--- perthtransport-lib/src/types/config.rs
+++ perthtransport-lib/src/types/config.rs
@@ -2,6 +2,7 @@
 pub struct ApplicationConfig {
     pub redis_connection_string: String,
     #[serde(default)]
+    // this isn't made available on the worker for obvious reasons
     pub worker_api_base: String,
     pub realtime_api_key: String,
     pub reference_data_api_key: String,
