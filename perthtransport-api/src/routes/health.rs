2d7ef4e662f9442a5f18dad242f7d96d68a297fb
Anurag Singh <contact@anurag.sh>
2023-07-01 23:01:47 +0800

    add timeout and error for status

--- perthtransport-api/src/routes/health.rs
+++ perthtransport-api/src/routes/health.rs
@@ -2,14 +2,17 @@ use crate::types::AppState;
 use axum::extract::State;
 use http::StatusCode;
 use perthtransport::types::health::{ServiceUnavailable, WorkerHealthStatus};
+use std::time::Duration;
 
 pub async fn health_check(State(state): State<AppState>) -> Result<StatusCode, ServiceUnavailable> {
     let health_endpoint = format!("{}/{}", state.config.worker_api_base, "status/health");
     let response = state
         .http_client
         .get(health_endpoint)
+        .timeout(Duration::from_secs(1))
         .send()
         .await?
+        .error_for_status()?
         .json::<WorkerHealthStatus>()
         .await?;
 
