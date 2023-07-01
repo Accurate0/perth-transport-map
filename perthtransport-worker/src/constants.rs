ab92c88443853f8e7f39d40cf40fe437ce70a98e
Anurag Singh <contact@anurag.sh>
2023-07-01 15:20:19 +0800

    remove unused file

--- perthtransport-worker/src/constants.rs
+++
@@ -1,6 +0,0 @@
-pub const CACHE_KEY_PREFIX: &str = "pta_trip";
-pub const DO_NOT_TRACK_KEY_PREFIX: &str = "pta_do_not_track";
-pub const ACTIVE_TRAINS_KEY: &str = "pta_active_trains";
-pub const PUBSUB_CHANNEL_WORKER_TASK_OUT: &str = "pta_worker_out";
-pub const TRANSPERTH_REAL_TIME_API: &str = "https://realtime.transperth.info/SJP/Trip";
-pub const TRANSPERTH_EARLY_HOURS: [u32; 5] = [0, 1, 2, 3, 4];
