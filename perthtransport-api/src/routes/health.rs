3e6825a2d22cd0ecf5e94fd082d6789578937a31
Anurag Singh <contact@anurag.sh>
2025-12-27 10:12:13 +0800

    update healthcheck

--- perthtransport-api/src/routes/health.rs
+++ perthtransport-api/src/routes/health.rs
@@ -1,29 +1,10 @@
 use crate::types::AppState;
 use axum::extract::State;
 use http::StatusCode;
-use perthtransport::types::health::{ServiceUnavailable, WorkerHealthStatus};
-use std::time::Duration;
+use perthtransport::types::health::ServiceUnavailable;
 
-pub async fn health_check(State(state): State<AppState>) -> Result<StatusCode, ServiceUnavailable> {
-    let health_endpoint = format!("{}/{}", state.config.worker_api_base, "status/health");
-    let response = state
-        .http_client
-        .get(health_endpoint)
-        .timeout(Duration::from_secs(3))
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<WorkerHealthStatus>()
-        .await?;
-
-    tracing::info!("response: {:?}", response);
-
-    if response.active_trains_healthy
-        && response.task_manager_healthy
-        && response.worker_output_healthy
-    {
-        Ok(StatusCode::NO_CONTENT)
-    } else {
-        Ok(StatusCode::SERVICE_UNAVAILABLE)
-    }
+pub async fn health_check(
+    State(_state): State<AppState>,
+) -> Result<StatusCode, ServiceUnavailable> {
+    Ok(StatusCode::NO_CONTENT)
 }
--- perthtransport-worker/src/routes/health.rs
+++ perthtransport-worker/src/routes/health.rs
@@ -1,10 +1,9 @@
 use crate::types::AppState;
-use axum::{extract::State, Json};
-use perthtransport::types::health::{ServiceUnavailable, WorkerHealthStatus};
+use axum::extract::State;
+use http::StatusCode;
+use perthtransport::types::health::ServiceUnavailable;
 
-pub async fn health_check(
-    State(state): State<AppState>,
-) -> Result<Json<WorkerHealthStatus>, ServiceUnavailable> {
+pub async fn health_check(State(state): State<AppState>) -> Result<StatusCode, ServiceUnavailable> {
     let active_trains_healthy = !state.active_trains_handle.is_finished();
     let worker_output_healthy = !state.worker_out_handle.is_finished();
     let task_manager_healthy =
@@ -17,9 +16,9 @@ pub async fn health_check(
         active_trains_healthy
     );
 
-    Ok(Json(WorkerHealthStatus {
-        worker_output_healthy,
-        task_manager_healthy,
-        active_trains_healthy,
-    }))
+    if active_trains_healthy && worker_output_healthy && task_manager_healthy {
+        Ok(StatusCode::OK)
+    } else {
+        Ok(StatusCode::SERVICE_UNAVAILABLE)
+    }
 }
